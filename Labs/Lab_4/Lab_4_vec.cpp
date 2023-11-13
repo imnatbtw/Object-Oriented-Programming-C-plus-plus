8bf57b7a90169879e748b8358b1527429f024a67
imnatbtw <146492896+imnatbtw@users.noreply.github.com>
2023-11-13 12:40:03 -0800

    test

---
+++ Labs/Lab_4/Lab_4_vec.cpp
@@ -0,0 +1,35 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab 4
+ * Nathalie Duran 007664638
+ * 30 October 2023
+ *
+ * This lab will show how to organize data by using arrays and vectors. It will display 
+ * the way both contain, access, and update elements and how to pass them into functions.
+ * These data structures allocate memory and their relation to pointers which will also be shown in this lab.
+ *
+ * The most challenging aspect was having many different file and understanding what each one does.
+ * After reviewing, I was able to understand why many files are necessary and what they individually mean.
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
+
