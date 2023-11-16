2f11f5784d951236583a4aa6c69237da9d994bd0
imnatbtw <146492896+imnatbtw@users.noreply.github.com>
2023-11-15 23:52:19 -0800

    update1

--- Labs/Lab_4/DisplayVec.cpp
+++ Labs/Lab_4/DisplayVec.cpp
@@ -1,42 +1,35 @@
 //DisplayVec.cpp
+
 #include "DisplayVec.h"
 
-/**
- * @brief Construct a new Display Vec:: Display Vec object
- * 
- */
 DisplayVec::DisplayVec(){
-
+    
 }
 
-/**
- * @brief prints the integers in v
- * 
- * @param v 
- */
 void DisplayVec::printVector(const vector<int> v){
     for(int i = 0; i < v.size(); i++){
-        cout << v [i] << endl;
+
     }
 }
 
-/**
- * @brief returns the memory size of the vector
- * 
- * @param v 
- * @return int 
- */
 int DisplayVec::getVectMemSize(const vector<int> v){
     int mem_size = 0;
     for(int i = 0; i < v.size(); i++){
-        mem_size += sizeof(v[i]); //mem_size = mem_size + sizeof(v[i]);
+        mem_size += sizeof(v[i]);
     }
     return mem_size;
 }
 
-DisplayVec::printMetaVector(const vector<int> v){
-
-}
 const int * DisplayVec::getAddress(vector<int> v, int index){
+    int * addr = &v[index];
+    return addr;
+}
 
+void DisplayVec::printMetaVector(const vector<int> v){
+    int mem_size = getVectMemSize(v);
+    int int_size = sizeof(v[0]);
+    printf("Vector has %lu number of integers. Each integer is worth %i bytes, totaling up to %i bytes of memory.\n", v.size(), int_size, mem_size);
+    printf("Element at index %i is located at (%p)\n", 0, getAddress(v, 0));
+    printf("Element at index %i is located at (%p)\n", 1, getAddress(v, 1));
+    printf("Element at index %lu is located at (%p)\n", v.size()-1 ,getAddress(v, v.size()-1));
 }
